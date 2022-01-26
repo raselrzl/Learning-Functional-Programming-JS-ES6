2f40721b61227933c47d6455c2f377c6375ac239
Rasel Miah <rasel6041@gmail.com>
2022-01-26 06:17:41 +0100

    arrow function

---
+++ ArrowFunction.js
@@ -0,0 +1,20 @@
+function fun1(arg1, arg2){
+
+}
+
+
+const fun1=(arg1, arg2)=>{
+    return arg1+arg2
+}
+
+const fun1=(arg1, arg2)=> arg1+arg2;
+
+const oneArg=number=>number*2;
+
+
+const noArg=()=>nconsole.log("hello")
+
+const ObjectExample =()=>({
+    name:"rasel",
+    age:29
+})
