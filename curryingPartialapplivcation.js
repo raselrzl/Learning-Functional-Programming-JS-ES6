09b7b023b30faf6c53cd5ddaed06a1b3f005b585
Rasel Miah <rasel6041@gmail.com>
2022-01-28 07:44:59 +0100

    currying

--- curryingPartialapplivcation.js
+++ curryingPartialapplivcation.js
@@ -13,9 +13,10 @@ const addPartial=x=>
     y=>
         z=>add(x,y,z);
 
-const add5=addPartial(5)
+
+const sum=addPartial(5)(6)(7);//currying 
+/* const add5=addPartial(5)
 const add5and6=add5(6)
-const sum=add5and6(7)
+const sum=add5and6(7) */
 console.log(sum)
 
-//currying 
