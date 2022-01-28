3cac1ef0efa5e74483260169907a05d9aadd9473
Rasel Miah <rasel6041@gmail.com>
2022-01-28 07:43:44 +0100

    currying and partial function

---
+++ curryingPartialapplivcation.js
@@ -0,0 +1,21 @@
+/* const add=(x,y,z)=>x+y+z
+
+const addPartial=(x,y)=>
+    z=>add(x,y,z);
+
+const add5and6=addPartial(5,6)
+const sum=add5and6(7) */
+
+
+
+add=(x,y,z)=>x+y+z
+const addPartial=x=>
+    y=>
+        z=>add(x,y,z);
+
+const add5=addPartial(5)
+const add5and6=add5(6)
+const sum=add5and6(7)
+console.log(sum)
+
+//currying 
